6e8f9a08cb0cad30d7c6380dc05bd6da06c35d2c
RNallamilli42 <ramteja.nallamilli@gmail.com>
2026-05-26 15:16:16 -0500

    Added base Entity class with a derived stat calculation

--- engine/models/entity.py
+++ engine/models/entity.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict, model_validator
 from enum import Enum 
+from typing import Self
 
 class ProficiencyRank(Enum):
     """Enums of the modifier for each proficiency level"""
@@ -76,6 +77,7 @@ class DamageTypes(Enum):
 
 class AbilityScores(BaseModel):
     """Represent the ability scores of an entity"""
+    model_config = ConfigDict(validate_assignment=True)
     strength : int = 0
     dexterity : int = 0
     constitution : int = 0
@@ -85,17 +87,20 @@ class AbilityScores(BaseModel):
 
 class TotalModifier(BaseModel):
     """Represents the total modifier given for a skill or save with proficiency, ability score, and item bonus"""
+    model_config = ConfigDict(validate_assignment=True)
     ability : str
     proficiency : ProficiencyRank = ProficiencyRank.UNTRAINED
     item_bonus : int = 0
 
 class LoreModifier(TotalModifier):
     """An extension of TotalModifier that allows Lores to have different names"""
+    model_config = ConfigDict(validate_assignment=True)
     name : str | None = None
     ability : str = "intelligence"
 
 class Skills(BaseModel):
     """Represents all of the entities different skills through TotalModifier and LoreModifier"""
+    model_config = ConfigDict(validate_assignment=True)
     acrobatics : TotalModifier = TotalModifier(ability = "dexterity")
     arcana : TotalModifier = TotalModifier(ability = "intelligence")
     athletics : TotalModifier = TotalModifier(ability = "strength")
@@ -116,14 +121,43 @@ class Skills(BaseModel):
 
 class Saves(BaseModel):
     """Represents an entity's different saves using TotalModifier"""
+    model_config = ConfigDict(validate_assignment=True)
     fortitude : TotalModifier = TotalModifier(ability = "constitution")
     reflex : TotalModifier = TotalModifier(ability = "dexterity")
     will : TotalModifier = TotalModifier(ability = "wisdom")
 
 class MovementSpeeds(BaseModel):
     """Represents an entity's different movement speeds"""
+    model_config = ConfigDict(validate_assignment=True)
     speed : int | None = None
     burrow_speed : int | None = None
     climb_speed : int | None = None
     fly_speed  : int | None = None
     swim_speed : int | None = None
+
+class Entity(BaseModel):
+    """Blueprint for a being in PF2e"""
+    model_config = ConfigDict(validate_assignment=True)
+    name : str
+    level : int = Field(0, ge = 0, le = 20)
+    size : Size
+    ability_scores : AbilityScores = AbilityScores()
+    saves : Saves = Saves()
+    skills : Skills = Skills()
+    movement : MovementSpeeds = MovementSpeeds()
+    current_hp : int = 0
+    max_hp : int = 0
+    ac: int = 0
+    perception : TotalModifier = TotalModifier(ability = "wisdom")
+    conditions : dict[Conditions, int | None] = Field(default_factory = dict)
+    resistances : dict[DamageTypes, int] = Field(default_factory = dict)
+    weaknesses : dict[DamageTypes, int] = Field(default_factory = dict)
+    actions : int = Field(3, ge = 0)
+    reactions : int = Field(1, ge = 0)
+
+    @model_validator(mode="after")
+    def set_derived_stats(self) -> Self:
+        """Function to set stats such as ac after Entity is created (may move to specific inherited classes)"""
+        self.ac = 10 + self.ability_scores.dexterity
+        return self
+    
